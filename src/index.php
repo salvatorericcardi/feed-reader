8b18d6b5038052780e64c5558efabc17813328b1
riccardi1997 <riccardi.salvatore.97@gmail.com>
2024-05-12 14:43:32 +0200

    fix: headers

--- src/index.php
+++ src/index.php
@@ -3,6 +3,7 @@
 require __DIR__ . "\\api\\feed.php";
 
 header('Access-Control-Allow-Origin: http://localhost:19006');
+header('Access-Control-Allow-Origin: https://feed-reader-six.vercel.app');
 
 $feed = new Feed();
 echo json_encode($feed->getFeed());
